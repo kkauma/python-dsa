98837cc37cbea3b95e8e6be0912d5559b713e8be
kkauma <kylekauma@gmail.com>
2023-09-11 21:40:18 -0600

    show the behavior of pointers with integers by looking at the addresses in memory

---
+++ Classes_Pointers/Pointers/pointers.py
@@ -0,0 +1,9 @@
+num1 = 11
+num2 = num1
+
+print("Before num2 value is updated:")
+print(f"num1 = {num1}")
+print(f"num2 = {num2}")
+
+print(f"\nnum1 points to {id(num1)}")
+print(f"num2 points to {id(num2)}")
