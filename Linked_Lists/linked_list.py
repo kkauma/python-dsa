523257e533d2a1f7a7118021b1edb43018da3bf8
kkauma <kylekauma@gmail.com>
2023-10-22 21:03:05 -0400

    add code for append method

--- Linked_Lists/linked_list.py
+++ Linked_Lists/linked_list.py
@@ -10,3 +10,14 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
