a04d2072f469e19275870f8a072d29eeb9afc49c
kkauma <kylekauma@gmail.com>
2023-10-31 22:07:25 -0400

    implement logic to remove a node from a linked list

--- Linked_Lists/linked_list.py
+++ Linked_Lists/linked_list.py
@@ -95,6 +95,20 @@ class LinkedList:
         self.length += 1
         return True
 
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
 
 my_linked_list = LinkedList(7)
 my_linked_list.append(4)
